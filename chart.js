49c339de467c763f190b4304f5d7b5ddcf38a204
Ryan Sharif <rsharif@g.ucla.edu>
2016-09-19 11:37:00 -0700

    added dynamic graph loader

--- chart.js
+++ chart.js
@@ -27,3 +27,67 @@ function combineDateAndUsage(dates, usage) {
     return dailyUsage
 }
 
+d3.json("data.json", (error, data) => {
+    // throw an error if data could not be loaded
+    if (error) throw error
+
+    let usage = data.day_stats
+    let dates = Object.keys(usage)
+	.map(key => Number(key))
+    let dailyUsage = Object.values(usage)
+    let dayAndUsage = combineDateAndUsage(dates, dailyUsage)
+
+    console.log(dayAndUsage)
+    let svg = d3.select("svg"),
+	margin = {top: 20, right: 20, bottom: 30, left: 40},
+	width = +svg.attr("width") - margin.left - margin.right,
+	height = +svg.attr("height") - margin.top - margin.bottom
+    
+    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+	y = d3.scaleLinear().rangeRound([height, 0])
+
+    let g = svg.append("g")
+	.attr("transform", "translate(" +
+	      margin.left + "," + margin.top + ")")
+    x.domain(dayAndUsage.map(d => d.day))
+    y.domain([0, d3.max(dayAndUsage, d => d.usage)])
+
+    g.append("g")
+	.attr("class", "axis axis--x")
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x))
+    
+    g.append("g")
+    	.attr("class", "axis axis--y")
+    	.call(d3.axisLeft(y).ticks(10))
+    	.append("text")
+    	.attr("transform", "rotate(-90)")
+    	.attr("y", 6)
+    	.attr("dy", "0.71em")
+    	.attr("text-anchor", "end")
+    	.text("Usage")
+
+    g.selectAll(".bar")
+    	.data(dayAndUsage)
+    	.enter().append("rect")
+    	.attr("class", "bar")
+    	.attr("x", d => x(d.day))
+	.attr("y", d => y(d.usage))
+	.attr("width", x.bandwidth())
+	.attr("height", d => height - y(d.usage))
+
+    let useSubtitle = d3.select("h2.useSubtitle")
+    useSubtitle.html(String(data.relevant_timespan.month_long + " usage data"))
+    
+    let monthUse = d3.select("div.monthUse")
+    monthUse.html("You used " + String(data.monthly_usage) +
+		     " watts this month.")
+
+    let weekdayAverage = d3.select("div.weekdayAverage")
+    weekdayAverage.html("Your weekday average was " +
+			String(data.weekday_average) + " watts.")
+
+    let averageDay = d3.select("div.averageDay")
+    averageDay.html("On average, you used "+
+		    String(data.average_day) + " watts per day.")
+})
